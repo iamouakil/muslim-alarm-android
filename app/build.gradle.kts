524434986e38f02c95882335bf8c106bbee8d3a5
iamouakil <ouakilbadaoui20@gmail.com>
2026-04-23 23:17:22 +0100

    fix: hardcode dependency versions in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,32 +36,28 @@ android {
 }
 
 dependencies {
-    val composeBomVersion = "2024.03.00"
-    val hiltVersion = "2.51"
-    val roomVersion = "2.6.1"
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:composeBomVersion"))
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("com.google.dagger:hilt-android:hiltVersion")
-    kapt("com.google.dagger:hilt-compiler:hiltVersion")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("androidx.room:room-runtime:roomVersion")
-    implementation("androidx.room:room-ktx:roomVersion")
-    kapt("androidx.room:room-compiler:roomVersion")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.google.dagger:hilt-android:2.51")
+    kapt("com.google.dagger:hilt-compiler:2.51")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:composeBomVersion"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
